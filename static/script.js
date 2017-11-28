243b3de2e952f7eff1287b79fe14f8e3ddfe21a0
Onur <onurburak@sabanciuniv.edu>
2017-11-28 11:50:28 -0800

    render into canvas feature added

--- static/script.js
+++ static/script.js
@@ -29,13 +29,19 @@ $(document).ready(function() {
         name: filename
       }
     }).done(function(response) {
-      var data = response.data
-      console.log(data);
+      var data = {}
+
+      for (var i = 0; i < response.data.length; i++) {
+        data[i] = {
+          description: response.data[i].description,
+          boundingPoly: response.data[i].boundingPoly
+        };
+      }
       appendHTML(data);
-      img = $("<figure/>").addClass("image is-128x128").append($("<img/>").attr("src", src));
+      setUpMenu(data);
+      img = $("<figure/>").addClass("image").append($("<img/>").attr("src", src));
       $(".image-cont").append(img)
     });
-
   }
 
   function appendHTML(data) {
@@ -43,6 +49,49 @@ $(document).ready(function() {
 
     regeStr = syntaxHighlight(jsonStr);
     $("pre").append(regeStr);
+  }
+
+  function setUpMenu(data) {
+    var x, y, maxX, maxY;
+    x = y = maxX = maxY = 0;
+    console.log(maxX, maxY);
+    var vertices = data["0"].boundingPoly.vertices
+    for (var i = 0; i < vertices.length; i++) {
+      x = vertices[i]["x"];
+      y = vertices[i]["y"];
+      if (x > maxX)
+        maxX = x;
+      if (y > maxY)
+        maxY = y
+    }
+
+    console.log("Max X : ", maxX);
+    console.log("Max Y : ", maxY);
+    console.log();
+
+    var canvas = document.getElementById("canvas");
+    canvas.height = maxY;
+    canvas.width = maxX;
+    var ctx = canvas.getContext("2d");
+    for (var i = 1; i < Object.keys(data).length; i++) {
+      var txt = data[i].description;
+      var verticeArray = data[i].boundingPoly.vertices;
+      var x = data[i].boundingPoly.vertices[0].x
+      var y = data[i].boundingPoly.vertices[0].y
+      var max = 0;
+      for (var z = 0; z < verticeArray.length; z++) {
+        xTemp = verticeArray[z]["x"];
+        if (xTemp > max)
+          max = xTemp;
+      }
+      width = max - x ;
+      ctx.font ="11px Arial"
+      ctx.fillStyle ="black"
+      console.log(txt,x,y,width);
+      ctx.fillText(txt,x,y,width);
+
+    }
+
   }
 
   function syntaxHighlight(json) {
